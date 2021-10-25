f286eef880b330c1d9119558019e7bc79b63e0b2
S-MW <72529306+S-MW@users.noreply.github.com>
2021-10-25 12:59:38 +0300

    Solve step2&3

--- AdvancedJs.js
+++ AdvancedJs.js
@@ -28,19 +28,14 @@
     //     }
 
 // Step 2: Rewrite the above code using an async/await function.
-// const request = async () =>  {  await fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699")
-//     const json =  (function(res){return res.json()} )
-//     const jsonAfter =   (function(result){
-//         jsonAfter= result.items;
-//         console.log(jsonAfter);
-//         }),
-//     // Note: it's important to handle errors here
-//     // instead of a catch() block so that we don't swallow
-//     // exceptions from actual bugs in components.
-//     const jsonError =  function(error){
-//     console.log(error);
-//     }
-// } 
+// Step 3: Copy and paste the URL for the API (https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699) into your browser to examine the data returned by google books. Now write the code to display (console log) only the title and description of the book returned.
+        const request = async () => {
+            const response = await fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699");
+            const json = await response.json();
+            console.log(json.items[0].volumeInfo.title );
+            console.log( json.items[0].volumeInfo.description );
+            }
+            request();
 
 
 
